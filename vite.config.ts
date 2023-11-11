d08257addeb697b283d13be3d3146ec5c9e1e2fe
Arthur Baileys Li <arthurlitovko@gmail.com>
2023-11-11 07:54:55 +0200

    ➕ Add lightning css

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { browserslistToTargets } from 'lightningcss';
+import browserslist from 'browserslist';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	css: {
+		transformer: 'lightningcss',
+		lightningcss: {
+			targets: browserslistToTargets(browserslist('defaults')),
+		},
+	},
+	build: {
+		cssMinify: 'lightningcss',
+	},
 });

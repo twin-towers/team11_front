add082be61e3d73ec4f33a8adbb8f837ddcf9353
Arthur Baileys Li <arthurlitovko@gmail.com>
2023-11-11 16:23:20 +0200

    ➕ Proof of concept canvas

---
+++ src/components/ProjectCanvas/Canvas.tsx
@@ -0,0 +1,27 @@
+import { useDroppable } from '@dnd-kit/core';
+import { useStore } from '@nanostores/react';
+
+import { $canvasItems } from '../../stores/active';
+import style from './canvas.module.css';
+
+export function Canvas() {
+	const { setNodeRef } = useDroppable({
+		id: 'canvas',
+	});
+
+	const active = useStore($canvasItems)
+	return (
+		<div className={style.body} ref={setNodeRef}>
+			{active.map((item) => (
+				<button
+					key={item.id}
+					style={{
+						transform: `translate3d(${item.coordinates.x}px, ${item.coordinates.y}px, 0)`,
+					}}
+				>
+					{item.body}
+				</button>
+			))}
+		</div>
+	);
+}

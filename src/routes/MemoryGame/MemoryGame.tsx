e49c8c0f9a8922e33adeda32bbd0b25358893038
Arthur Baileys Li <arthurlitovko@gmail.com>
2023-11-12 15:13:47 +0200

    Add gradients

--- src/routes/MemoryGame/MemoryGame.tsx
+++ src/routes/MemoryGame/MemoryGame.tsx
@@ -3,12 +3,13 @@ import { useStore } from '@nanostores/react';
 import { MemoryToolbar } from '../../components/MemoryToolbar/MemoryToolbar';
 import { $isFinished } from '../../stores/memory';
 import { GameBoard } from './../../components/GameBoard/GameBoard';
+import style from './style.module.css';
 
 export function MemoryGame() {
 	const isFinished = useStore($isFinished);
 
 	return (
-		<main>
+		<main className={style.host}>
 			<MemoryToolbar />
 			<GameBoard />
 
